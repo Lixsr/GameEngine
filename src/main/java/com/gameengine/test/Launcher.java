31d096d49cfd4b003930dee6583d7b9bb7c477a4
Lixsr <osama142494@gmail.com>
2025-04-07 01:17:57 +0300

    Windwo Setup

--- src/main/java/com/gameengine/test/Launcher.java
+++ src/main/java/com/gameengine/test/Launcher.java
@@ -1,9 +1,15 @@
 package com.gameengine.test;
 
+import com.gameengine.core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println(Version.getVersion());
+        WindowManager window = new WindowManager("GameEngine",1600, 900, false);
+        window.init();
+        while(!window.windowShouldClose()){
+            window.update();
+        }
     }
 }
