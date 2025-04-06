e6659414c29ace1d3a05b501639cc13580a802d8
Lixsr <osama142494@gmail.com>
2025-04-07 00:35:15 +0300

    Setup

---
+++ src/main/java/com/gameengine/test/Launcher.java
@@ -0,0 +1,9 @@
+package com.gameengine.test;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println(Version.getVersion());
+    }
+}
