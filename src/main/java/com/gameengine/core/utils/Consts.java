8b833c5906b8cff538c9fd0e42a147f09f8afa78
Lixsr <osama142494@gmail.com>
2025-04-12 00:55:43 +0300

    GameEngine loop in EngineMange

---
+++ src/main/java/com/gameengine/core/utils/Consts.java
@@ -0,0 +1,16 @@
+package com.gameengine.core.utils;
+
+public class Consts {
+    public static final String TITLE = "GameEngine";
+
+    public static final float FOV = (float) Math.toRadians(60);
+//    public static final float Z_NEAR = 0.01f;
+//    public static final float Z_FAR = 1000f;
+//
+//    public static final float SPECULAR_POWER = 10f;
+//
+//    public static final float MOUSE_SENSITIVITY = 0.2f;
+//    public static final float CAMERA_STEP = 0.05f;
+
+
+}
--- src/main/java/com/gameengine/test/Launcher.java
+++ src/main/java/com/gameengine/test/Launcher.java
@@ -1,15 +1,25 @@
 package com.gameengine.test;
 
+import com.gameengine.core.EngineManager;
 import com.gameengine.core.WindowManager;
+import com.gameengine.core.utils.Consts;
 import org.lwjgl.Version;
 
 public class Launcher {
+    private static WindowManager window;
+    private static EngineManager engine;
     public static void main(String[] args) {
-        System.out.println(Version.getVersion());
-        WindowManager window = new WindowManager("GameEngine",1600, 900, false);
-        window.init();
-        while(!window.windowShouldClose()){
-            window.update();
+        window = new WindowManager(Consts.TITLE,1600, 900, false);
+        engine = new EngineManager();
+
+        try {
+            engine.start();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public static WindowManager getWindow() {
+        return window;
     }
 }
