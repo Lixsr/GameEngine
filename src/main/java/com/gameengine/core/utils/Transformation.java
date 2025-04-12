6d763c078ae6b6774d85fca22f192cf43e03f69e
Lixsr <osama142494@gmail.com>
2025-04-12 13:52:06 +0300

    camera movement & render minecraft block

--- src/main/java/com/gameengine/core/utils/Transformation.java
+++ src/main/java/com/gameengine/core/utils/Transformation.java
@@ -1,5 +1,6 @@
 package com.gameengine.core.utils;
 
+import com.gameengine.core.Camera;
 import com.gameengine.core.entity.Entity;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
@@ -15,15 +16,15 @@ public class Transformation {
         return matrix;
     }
 
-//    public static Matrix4f getViewMatrix(Camera camera){
-//        Vector3f pos = camera.getPosition();
-//        Vector3f rot = camera.getRotation();
-//        Matrix4f matrix = new Matrix4f();
-//        matrix.identity();
-//        matrix.rotate((float) Math.toRadians(rot.x), new Vector3f(1, 0, 0))
-//                .rotate((float) Math.toRadians(rot.y), new Vector3f(0, 1, 0))
-//                .rotate((float) Math.toRadians(rot.z), new Vector3f(0, 0, 1));
-//        matrix.translate(-pos.x, -pos.y, -pos.z);
-//        return matrix;
-//    }
+    public static Matrix4f getViewMatrix(Camera camera){
+        Vector3f pos = camera.getPosition();
+        Vector3f rot = camera.getRotation();
+        Matrix4f matrix = new Matrix4f();
+        matrix.identity();
+        matrix.rotate((float) Math.toRadians(rot.x), new Vector3f(1, 0, 0))
+                .rotate((float) Math.toRadians(rot.y), new Vector3f(0, 1, 0))
+                .rotate((float) Math.toRadians(rot.z), new Vector3f(0, 0, 1));
+        matrix.translate(-pos.x, -pos.y, -pos.z);
+        return matrix;
+    }
 }
